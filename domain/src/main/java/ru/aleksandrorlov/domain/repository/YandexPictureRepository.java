9f7a5f0ff9062b75b6a1f885c6644524e99568d6
Alexander Orlov <79202875363@ya.ru>
2018-03-15 12:57:22 +0300

    domain - создал модель и интерфейс репозитория

---
+++ domain/src/main/java/ru/aleksandrorlov/domain/repository/YandexPictureRepository.java
@@ -0,0 +1,16 @@
+package ru.aleksandrorlov.domain.repository;
+
+import java.util.List;
+import io.reactivex.Observable;
+import ru.aleksandrorlov.domain.YandexPicture;
+
+/**
+ * Created by alex on 15.03.18.
+ */
+
+public interface YandexPictureRepository {
+
+    Observable<List<YandexPicture>> yandexPictures();
+
+    Observable<YandexPicture> yandexPicture(final int yandexPictureId);
+}
