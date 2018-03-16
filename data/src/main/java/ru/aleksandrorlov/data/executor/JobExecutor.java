ad5e462b7704946aef26f03092ad8799db5e007f
Alexander Orlov <79202875363@ya.ru>
2018-03-16 11:45:54 +0300

    Создал Executor

---
+++ data/src/main/java/ru/aleksandrorlov/data/executor/JobExecutor.java
@@ -0,0 +1,10 @@
+package ru.aleksandrorlov.data.executor;
+
+import ru.aleksandrorlov.domain.executor.ThreadExecutor;
+
+/**
+ * Created by alex on 16.03.18.
+ */
+
+public class JobExecutor implements ThreadExecutor {
+}
