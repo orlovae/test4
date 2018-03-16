73659921811d9fe6d8a74249c0dbf8fb786dd335
Alexander Orlov <79202875363@ya.ru>
2018-03-16 13:04:56 +0300

    Реализовал модель для работы с базой данных

--- data/src/main/java/ru/aleksandrorlov/data/entity/mapper/YandexPictureDBModel.java
+++ data/src/main/java/ru/aleksandrorlov/data/entity/mapper/YandexPictureDBModel.java
@@ -6,5 +6,15 @@ import com.orm.SugarRecord;
  * Created by alex on 16.03.18.
  */
 
-public class YandexPictureDBModel extends SugarRecord {
+public class YandexPictureDBModel extends SugarRecord<YandexPictureDBModel> {
+    String url;
+    String request;
+
+    public YandexPictureDBModel() {
+    }
+
+    public YandexPictureDBModel(String url, String request) {
+        this.url = url;
+        this.request = request;
+    }
 }
