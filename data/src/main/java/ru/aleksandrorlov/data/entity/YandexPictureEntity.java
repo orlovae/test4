04dd073b90d62fb06d6a3307c83399bef58eb0d4
Alexander Orlov <79202875363@ya.ru>
2018-03-16 11:35:19 +0300

    Создал модель и маппер

---
+++ data/src/main/java/ru/aleksandrorlov/data/entity/YandexPictureEntity.java
@@ -0,0 +1,8 @@
+package ru.aleksandrorlov.data.entity;
+
+/**
+ * Created by alex on 16.03.18.
+ */
+
+public class YandexPictureEntity {
+}
---
+++ data/src/main/java/ru/aleksandrorlov/data/entity/mapper/YandexPictureEntityDataMapper.java
@@ -0,0 +1,8 @@
+package ru.aleksandrorlov.data.entity.mapper;
+
+/**
+ * Created by alex on 16.03.18.
+ */
+
+public class YandexPictureEntityDataMapper {
+}
