66caaee9ec522d777bd189724b15951002e1d418
Alexander Orlov <79202875363@ya.ru>
2018-03-16 12:58:37 +0300

    Подключил библиотеку с бд

--- data/src/main/java/ru/aleksandrorlov/data/database/YandexPictureDataBase.java
+++ data/src/main/java/ru/aleksandrorlov/data/database/YandexPictureDataBase.java
@@ -1,8 +1,18 @@
 package ru.aleksandrorlov.data.database;
 
+import java.util.List;
+
+import ru.aleksandrorlov.data.entity.YandexPictureEntity;
+
 /**
  * Created by alex on 16.03.18.
  */
 
 public interface YandexPictureDataBase {
+    void put(YandexPictureEntity yandexPictureEntity);
+    void update(final int yandexPictureId, YandexPictureEntity yandexPictureEntity);
+    void delete(final int yandexPictureId);
+
+    YandexPictureEntity getYandexPicture(final int yandexPictureId);
+    List<YandexPictureEntity> getYandexPictureList();
 }
