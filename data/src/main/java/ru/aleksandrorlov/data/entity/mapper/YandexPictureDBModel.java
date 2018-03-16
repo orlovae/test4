36127cc781debec9f8976bb9e18c3c37560d7061
Alexander Orlov <79202875363@ya.ru>
2018-03-16 13:01:17 +0300

    Создал модель для базы данных

---
+++ data/src/main/java/ru/aleksandrorlov/data/entity/mapper/YandexPictureDBModel.java
@@ -0,0 +1,10 @@
+package ru.aleksandrorlov.data.entity.mapper;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by alex on 16.03.18.
+ */
+
+public class YandexPictureDBModel extends SugarRecord {
+}
