6bebdbf8e0b0dd12dea79b8d18f22c54db8fb635
Alexander Orlov <79202875363@ya.ru>
2018-03-16 12:26:02 +0300

    Создал классы репозитория

---
+++ data/src/main/java/ru/aleksandrorlov/data/repository/YandexPictureDataRepository.java
@@ -0,0 +1,10 @@
+package ru.aleksandrorlov.data.repository;
+
+import ru.aleksandrorlov.domain.repository.YandexPictureRepository;
+
+/**
+ * Created by alex on 16.03.18.
+ */
+
+public class YandexPictureRepository implements YandexPictureRepository {
+}
---
+++ data/src/main/java/ru/aleksandrorlov/data/repository/datasource/DBYandexPictureDataStore.java
@@ -0,0 +1,8 @@
+package ru.aleksandrorlov.data.repository.datasource;
+
+/**
+ * Created by alex on 16.03.18.
+ */
+
+public class DBYandexPictureDataStore implements YandexPictureDataStore {
+}
