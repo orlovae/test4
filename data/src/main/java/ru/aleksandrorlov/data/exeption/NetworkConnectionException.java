d2f95a07ba77f5166de6d8bf7743c1ff2ef8f8de
Alexander Orlov <79202875363@ya.ru>
2018-03-16 11:49:56 +0300

    Создал Exeption

---
+++ data/src/main/java/ru/aleksandrorlov/data/exeption/NetworkConnectionException.java
@@ -0,0 +1,8 @@
+package ru.aleksandrorlov.data.exeption;
+
+/**
+ * Created by alex on 16.03.18.
+ */
+
+public class NetworkConnectionException extends Exception {
+}
---
+++ data/src/main/java/ru/aleksandrorlov/data/exeption/RepositoryErrorBundle.java
@@ -0,0 +1,10 @@
+package ru.aleksandrorlov.data.exeption;
+
+import ru.aleksandrorlov.domain.exception.ErrorBundle;
+
+/**
+ * Created by alex on 16.03.18.
+ */
+
+public class RepositoryErrorBundle implements ErrorBundle {
+}
