a75798e1784d2bafd28b1455ee783ea22ffd9688
Alexander Orlov <79202875363@ya.ru>
2018-03-17 17:43:17 +0300

    Создал класс диалога

---
+++ presentation/src/main/java/ru/aleksandrorlov/test4/view/fragment/Dialog.java
@@ -0,0 +1,10 @@
+package ru.aleksandrorlov.test4.view.fragment;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by alex on 17.03.18.
+ */
+
+public class Dialog extends DialogFragment {
+}
