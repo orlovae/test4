b5bdc1f13f32dc9820c828f30f306f034929c712
Alexander Orlov <79202875363@ya.ru>
2018-03-17 17:16:50 +0300

    Реализовал классы для работы с FAB

--- presentation/src/main/java/ru/aleksandrorlov/test4/view/activity/MainActivity.java
+++ presentation/src/main/java/ru/aleksandrorlov/test4/view/activity/MainActivity.java
@@ -1,15 +1,45 @@
 package ru.aleksandrorlov.test4.view.activity;
 
+
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 
 import ru.aleksandrorlov.test4.R;
+import ru.aleksandrorlov.test4.view.activity.view.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFAB();
+        FABBehavior();
+    }
+
+    private void initFAB() {
+        fab = new FloatingActionButton.Builder(this)
+                .withDrawable(getResources().getDrawable(R.drawable.ic_add_black_24dp))
+                .withButtonColor(Color.WHITE)
+                .withGravity(Gravity.BOTTOM | Gravity.RIGHT)
+                .withMargins(0, 0, 16, 16)
+                .create();
+    }
+
+    private void FABBehavior() {
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab:
+                break;
+        }
     }
 }
