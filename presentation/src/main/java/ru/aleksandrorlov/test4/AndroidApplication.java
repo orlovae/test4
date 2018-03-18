4586759fbc250ba5b6498a29a34d27e2177d9475
Alexander Orlov <79202875363@ya.ru>
2018-03-18 14:04:43 +0300

    Создал классы для di

---
+++ presentation/src/main/java/ru/aleksandrorlov/test4/AndroidApplication.java
@@ -0,0 +1,10 @@
+package ru.aleksandrorlov.test4;
+
+import android.app.Application;
+
+/**
+ * Created by alex on 18.03.18.
+ */
+
+public class AndroidApplication extends Application {
+}
---
+++ presentation/src/main/java/ru/aleksandrorlov/test4/di/modules/ActivityModule.java
@@ -0,0 +1,8 @@
+package ru.aleksandrorlov.test4.di;
+
+/**
+ * Created by alex on 18.03.18.
+ */
+
+public class ActivityModule {
+}
